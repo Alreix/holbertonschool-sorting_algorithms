9dc4a1b927a98e4975746374f39c487542eff871
Alreix <alreixdev@gmail.com>
2025-12-09 15:14:15 +0100

    fix: the order and placement of protoype for print_list and print_array

--- sort.h
+++ sort.h
@@ -1,20 +1,6 @@
 #ifndef SORT_H
-#define SORT_T
-
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size);
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list);
+#define SORT_H
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -25,13 +11,16 @@ void print_list(const listint_t *list);
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
 #endif
