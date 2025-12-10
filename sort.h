8b40bcb0e1eca29e13f201cbf8aaa5ee5896887a
Bengin05 <uzunbeengin9@gmail.com>
2025-12-10 11:34:47 +0000

    modification the file

--- sort.h
+++ sort.h
@@ -1,5 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
+
 #include <stddef.h>
 
 /**
@@ -11,13 +12,14 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -25,4 +27,4 @@ void quick_sort(int *array, size_t size);
 void _quick_sort(int *array, int low, int high, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 
-#endif
+#endif /* SORT_H */
